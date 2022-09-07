e671db357d31241b8fe0bbf6cc7a9d4e4fa43ab9
thain <thain@HONG-THAI>
2022-09-08 00:14:57 +0700

    Add project files.

---
+++ Practice2-S5-Data Annotation/Controllers/UserController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Practice2_S5_Data_Annotation.Models;
+
+namespace Practice2_S5_Data_Annotation.Controllers
+{
+    public class UserController : Controller
+    {
+        private static List<User> _users = new List<User>()
+        {
+            new User("1", "Thai", "123123", "123123", "thai@gmail.com"),
+            new User("2", "Thai2", "123123", "123123", "thai@gmail.com"),
+            new User("3", "Thai3", "123123", "123123", "thai@gmail.com"),
+        };
+
+        // GET: User
+        public ActionResult Index()
+        {
+
+            var model = _users;
+            return View(model);
+        }
+
+        // GET: User details
+        public ActionResult Details(string Id)
+        {
+            if (Id != null)
+            {
+                foreach (var user in _users)
+                {
+                    if (user.Id == Id)
+                    {
+                        return View(user);
+                    }   
+                }
+            }
+            return View();
+        }
+
+        
+        public ActionResult Create()
+        {
+            User user = new User();
+            return View("Create", user);
+        }
+
+        //POST: User
+        [HttpPost]
+        public ActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                User model = new User();
+                model.Id = user.Id;
+                model.Name = user.Name;
+                model.Email = user.Email;
+                model.Password = user.Password;
+                _users.Add(model);
+            }
+
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Edit(string Id)
+        {
+            if (Id != null)
+            {
+                foreach (var user in _users)
+                {
+                    if (user.Id == Id)
+                    {
+                        return View("Edit", user);
+                    }
+                }
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Edit(User user)
+        {
+            if (ModelState.IsValid)
+            {
+                User model = new User();
+                model.Id = user.Id;
+                model.Name = user.Name;
+                model.Email = user.Email;
+                model.Password = user.Password;
+                _users.Add(model);
+                _users.Remove(user);
+            }
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(string Id)
+        {
+            if (Id != null)
+            {
+                foreach (var user in _users)
+                {
+                    if (user.Id == Id)
+                    {
+                        return View("Delete", user);
+                    }
+                }
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Delete(User user)
+        {
+            _users.Remove(user);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Practice2-S5-Data Annotation/Models/User.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace Practice2_S5_Data_Annotation.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        [Required]
+        [StringLength(20, MinimumLength = 3, ErrorMessage = "Please enter your name")]
+        public string Name { get; set; }
+        [Required]
+        [MinLength(8, ErrorMessage = "Password must have at least 8 character")]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        [Compare("Password")]
+        public string ConfirmPassword { get; set; }
+        [Required]
+        [RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$", ErrorMessage = "Please enter valid email")]
+        public string Email { get; set; }
+
+        public User(string id, string name, string password, string confirmPassword, string email)
+        {
+            Id = id;
+            Name = name;
+            Password = password;
+            ConfirmPassword = confirmPassword;
+            Email = email;
+        }
+
+        public User()
+        {
+        }
+    }
+}
